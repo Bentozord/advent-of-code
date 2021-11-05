f35356a50e9b38d7cddc0f2abbd7bcde55479e55
Bentozord <rafal.utnicki26@gmail.com>
2021-11-05 18:09:03 +0100

    aoc day 14, more functional approach

--- src/main/java/twenty/day/fourteen/DockingCoordinator.java
+++ src/main/java/twenty/day/fourteen/DockingCoordinator.java
@@ -1,12 +1,15 @@
 package twenty.day.fourteen;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class DockingCoordinator {
 
@@ -17,30 +20,34 @@ class DockingCoordinator {
     }
 
     Map<Long, Long> initializeDockingProgramWithAddressChange(List<DockingInstruction> dockingInstructions) {
-        Map<Long, Long> memoryMap = new HashMap<>();
-        for (DockingInstruction dockingInstruction : dockingInstructions) {
-            for (MemoryInstruction memoryInstruction : dockingInstruction.memoryInstructions()) {
-                final List<Long> possibleAddresses = calculateAddressesChange(dockingInstruction.mask(), memoryInstruction.memoryAddress());
-                for (Long address : possibleAddresses) {
-                    memoryMap.put(address, memoryInstruction.value());
-                }
-            }
-        }
-        return memoryMap;
+        return dockingInstructions.stream()
+                .flatMap(this::memoryInstructionAddressChanges)
+                .collect(Collectors.toMap(Pair::getLeft, Pair::getRight, (a1, a2) -> a2));
+    }
+
+    private Stream<Pair<Long, Long>> memoryInstructionAddressChanges(DockingInstruction dockingInstruction) {
+        return dockingInstruction.memoryInstructions().stream()
+                .flatMap(memoryInstruction -> {
+                    final List<Long> longs = calculateAddressesChange(dockingInstruction.mask(), memoryInstruction.memoryAddress());
+                    return longs.stream()
+                            .map(aLong -> Pair.of(aLong, memoryInstruction.value()));
+                });
     }
 
     Map<Long, Long> initializeDockingProgram(List<DockingInstruction> dockingInstructions) {
-        Map<Long, Long> memoryMap = new HashMap<>();
-        dockingInstructions
-                .forEach(dockingInstruction ->
-                        dockingInstruction.memoryInstructions()
-                                .forEach(memoryInstruction ->
-                                        memoryMap.put(
-                                                memoryInstruction.memoryAddress(),
-                                                calculateValueChange(dockingInstruction.mask(), memoryInstruction.value())
-                                        )));
-        return memoryMap;
+        return dockingInstructions.stream()
+                .flatMap(this::memoryInstructionValueChanges)
+                .collect(Collectors.toMap(Pair::getLeft, Pair::getRight, (a1, a2) -> a2));
     }
+
+    private Stream<Pair<Long, Long>> memoryInstructionValueChanges(DockingInstruction dockingInstruction) {
+        return dockingInstruction.memoryInstructions().stream()
+                .map(memoryInstruction -> Pair.of(
+                        memoryInstruction.memoryAddress(),
+                        calculateValueChange(dockingInstruction.mask(), memoryInstruction.value())
+                ));
+    }
+
 
     Long calculateValueChange(String mask, Long value) {
         final String binaryValue = StringUtils.leftPad(Long.toBinaryString(value), 36, '0');
